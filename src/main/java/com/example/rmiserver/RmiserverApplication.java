97ef59bdd55b8a3bcd57eeee87e37f1964bcb3ac
sharon kirigo <sharonkirigo@gmail.com>
2019-10-29 18:21:55 +0300

    initial commit

---
+++ src/main/java/com/example/rmiserver/RmiserverApplication.java
@@ -0,0 +1,13 @@
+package com.example.rmiserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RmiserverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RmiserverApplication.class, args);
+	}
+
+}
